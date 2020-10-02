42eec8225d9e658cb44254f35558a21c4c75676f
Stanislav Alexandrov <stanislavalexandrov@gmail.com>
2020-10-02 21:32:25 +0500

    feature/added-second-mode

--- js/index.js
+++ js/index.js
@@ -4,3 +4,14 @@ function menuToggle() {
     toggleMenu.classList.toggle('active');
     navigation.classList.toggle('active');
 }
+
+function themeToggle() {
+    let toggleTheme = document.querySelector('.toggleTheme');
+    if (toggleTheme.innerText == 'regular mode') {
+        toggleTheme.innerText = 'dark mode';
+    } else {
+        toggleTheme.innerText = 'regular mode';
+    }
+
+    document.body.classList.toggle('dark-theme');
+}
